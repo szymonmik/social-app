5806caec70d8effdbb759032e4a2a57c910cd709
szymonmik <szyymiik@gmail.com>
2023-06-12 22:17:04 +0200

    extended user entity

--- API/Entities/User.cs
+++ API/Entities/User.cs
@@ -1,4 +1,7 @@
-﻿namespace API.Entities;
+﻿using API.Extensions;
+using Microsoft.AspNetCore.Identity;
+
+namespace API.Entities;
 
 public class User
 {
@@ -9,5 +12,18 @@ public class User
 	public string FirstName { get; set; }
 	public string SecondName { get; set; }
 	public string Surname { get; set; }
+	public DateOnly DateOfBirth { get; set; }
+	public DateTime Created { get; set; } = DateTime.UtcNow;
+	public DateTime LastActive { get; set; } = DateTime.UtcNow;
+	public string Gender { get; set; }
+	public string Introduction { get; set; }
+	public string Interests { get; set; }
+	public string City { get; set; }
+	public string Country { get; set; }
+	public List<Photo> Photos { get; set; } = new();
 
+	public int GetAge()
+	{
+		return DateOfBirth.CalculateAge();
+	}
 }
